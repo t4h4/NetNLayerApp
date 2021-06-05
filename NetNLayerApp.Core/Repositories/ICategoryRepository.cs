2f70828ae532a1b8731b6e9c02225d801d67ee77
t4h4 <tahayasin@outlook.com>
2021-06-05 17:04:56 +0300

    Added ICategoryRepository generic interface ICategoryRepository generic interface yapısı oluşturuldu.

---
+++ NetNLayerApp.Core/Repositories/ICategoryRepository.cs
@@ -0,0 +1,14 @@
+﻿using NetNLayerApp.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetNLayerApp.Core.Repositories
+{
+    interface ICategoryRepository : IRepository<Category> //IRepository'ye generic olarak Category veriliyor
+    {
+        Task<Category> GetWithProductsByIdAsync(int categoryId); //category'e bagli urunlerin donmesi beklenmekte 
+    }
+}
