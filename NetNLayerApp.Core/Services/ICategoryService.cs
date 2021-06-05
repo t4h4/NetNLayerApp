b43a79a58a05a6501a61e5c6933f54b04735d78e
t4h4 <tahayasin@outlook.com>
2021-06-05 23:19:54 +0300

    Created ICategoryService generic interface ICategoryService generic interface yapısı oluşturuldu.

---
+++ NetNLayerApp.Core/Services/ICategoryService.cs
@@ -0,0 +1,15 @@
+﻿using NetNLayerApp.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetNLayerApp.Core.Services
+{
+    interface ICategoryService : IService<Category>
+    {
+        Task<Category> GetWithProductsByIdAsync(int categoryId);
+
+    }
+}
