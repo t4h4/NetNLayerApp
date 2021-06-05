ed87fbd0b71f24ed5df6756b3f01b884c38ec4fb
t4h4 <tahayasin@outlook.com>
2021-06-05 16:23:45 +0300

    Created product and category models Product ve category model (entity) yapıları oluşturuldu.

---
+++ NetNLayerApp.Core/Models/Category.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetNLayerApp.Core.Models
+{
+    class Category
+    {
+        public Category() //category olusturuldugunda bos bir product collection nesnesi
+        {
+            Products = new Collection<Product>();
+        }
+
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public bool IsDeleted { get; set; } //veritabanindan silmek yerine silinme durumunu veritabaninda tutulacak
+
+        public ICollection<Product> Products { get; set; } //her kategori birden fazla urune sahip olabilir. one to many relation
+
+    }
+}
