44e11a0839cf08625869d7a4d58ffe63685fa5aa
t4h4 <tahayasin@outlook.com>
2021-06-05 16:53:35 +0300

    Added IRepository generic interface IRepository generic interface yapısı oluşturuldu.

---
+++ NetNLayerApp.Core/Repositories/IRepository.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetNLayerApp.Core.Repositories
+{
+    interface IRepository<TEntity> where TEntity : class //generic bir repository. where TEntity : class (TEntity mutlaka class olsun)
+    {
+        Task<TEntity> GetByIdAsync(int id);
+
+        Task<IEnumerable<TEntity>> GetAllAsync();
+
+        Task<IEnumerable<TEntity>> Where(Expression<Func<TEntity, bool>> predicate); //TEntity alan, geriye bool donen. herhangi bir parametreye gore ilgili nesneleri bulma
+
+        //category.SingleOrDefaultAsync(x=>x.name="kalem")
+        Task<TEntity> SingleOrDefaultAsync(Expression<Func<TEntity, bool>> predicate);
+
+        Task AddAsync(TEntity entity);
+
+        Task AddRangeAsync(IEnumerable<TEntity> entities); //coklu kayit
+
+        void Remove(TEntity entity);
+
+        void RemoveRange(IEnumerable<TEntity> entities);
+
+        TEntity Update(TEntity entity);
+
+    }
+}
