a7c58ff9ee29c0899631c85f4da44340ce35d0aa
t4h4 <tahayasin@outlook.com>
2021-06-05 17:11:29 +0300

    Added IProductRepository generic interface IProductRepository generic interface yapısı oluşturuldu.

---
+++ NetNLayerApp.Core/Repositories/IProductRepository.cs
@@ -0,0 +1,14 @@
+﻿using NetNLayerApp.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetNLayerApp.Core.Repositories
+{
+    interface IProductRepository : IRepository<Product>
+    {
+        Task<Product> GetWithCategoryByIdAsync(int productId); //producId'ye gore urunun kategorisi. 
+    }
+}
