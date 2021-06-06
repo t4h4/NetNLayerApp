05cdcbc8b51ccd64cf6708a86af318b59aee8017
t4h4 <tahayasin@outlook.com>
2021-06-06 23:11:45 +0300

    Created UnitOfWork class

---
+++ NetNLayerApp.Data/UnitOfWorks/UnitOfWork.cs
@@ -0,0 +1,38 @@
+﻿using NetNLayerApp.Core.Repositories;
+using NetNLayerApp.Core.UnitOfWorks;
+using NetNLayerApp.Data.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetNLayerApp.Data.UnitOfWorks
+{
+    class UnitOfWork : IUnitOfWork
+    {
+        private readonly AppDbContext _context;
+
+        private ProductRepository _productRepository;
+        private CategoryRepository _CategoryRepository;
+
+        public IProductRepository Products => _productRepository = _productRepository ?? new ProductRepository(_context); //_productRepository varsa al eğer null ise yeni bir ProductRepository(_context) olustur sonra _productRepository' ata. _productRepository'yıda Products'a ata.
+
+        public ICategoryRepository categories => _CategoryRepository = _CategoryRepository ?? new CategoryRepository(_context);
+
+        public UnitOfWork(AppDbContext appDbContext)
+        {
+            _context = appDbContext;
+        }
+
+        public void Commit()
+        {
+            _context.SaveChanges();
+        }
+
+        public async Task CommitAsync()
+        {
+            await _context.SaveChangesAsync();
+        }
+    }
+}
