7e9e320a868c58c702a3224b0bc38012f38cb541
t4h4 <tahayasin@outlook.com>
2021-06-06 17:09:14 +0300

    Created AppDbContext class Base DbContext

---
+++ NetNLayerApp.Data/AppDbContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NetNLayerApp.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetNLayerApp.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) //options base class (DbContext)'e gonderildi. 
+        {
+        }
+
+        public DbSet<Category> Categories { get; set; }
+
+        public DbSet<Product> Products { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+        }
+    }
+}
