cf0bd20eb9a4cd9804c797ca24c8cc4390678a78
t4h4 <tahayasin@outlook.com>
2021-06-06 16:40:46 +0300

    Created IUnitOfWork interface IUnitOfWork interface yapısı oluşturuldu.

---
+++ NetNLayerApp.Core/UnitOfWorks/IUnitOfWork.cs
@@ -0,0 +1,20 @@
+﻿using NetNLayerApp.Core.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetNLayerApp.Core.UnitOfWorks
+{
+    interface IUnitOfWork
+    {
+        IProductRepository Products { get; }
+
+        ICategoryRepository categories { get; }
+
+        Task CommitAsync(); //implemente edildiginde ef tarafinda savechange metodunu cagirir. 
+
+        void Commit();
+    }
+}
