469130a416e4f872a6799e72aff519ecd1b2a245
t4h4 <tahayasin@outlook.com>
2021-06-06 22:54:00 +0300

    Created CategoryRepository class

---
+++ NetNLayerApp.Data/Repositories/CategoryRepository.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NetNLayerApp.Core.Models;
+using NetNLayerApp.Core.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetNLayerApp.Data.Repositories
+{
+    class CategoryRepository : Repository<Category>, ICategoryRepository
+    {
+        private AppDbContext _appDbContext { get => _context as AppDbContext; }
+
+        public CategoryRepository(AppDbContext context) : base(context)
+        {
+        }
+
+        public async Task<Category> GetWithProductsByIdAsync(int categoryId)
+        {
+            return await _appDbContext.Categories.Include(x => x.Products).SingleOrDefaultAsync(x => x.Id == categoryId);
+        }
+    }
+}
