8a806cc9a689f17f4412a6c6411e597474b674ab
t4h4 <tahayasin@outlook.com>
2021-06-09 23:59:12 +0300

    Created services class

---
+++ NetNLayerApp.Service/Services/CategoryService.cs
@@ -0,0 +1,23 @@
+﻿using NetNLayerApp.Core.Models;
+using NetNLayerApp.Core.Repositories;
+using NetNLayerApp.Core.Services;
+using NetNLayerApp.Core.UnitOfWorks;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetNLayerApp.Service.Services
+{
+    public class CategoryService : Service<Category>, ICategoryService
+    {
+        public CategoryService(IUnitOfWork unitOfWork, IRepository<Category> repository) : base(unitOfWork, repository)
+        {
+        }
+        public async Task<Category> GetWithProductsByIdAsync(int categoryId)
+        {
+            return await _unitOfWork.categories.GetWithProductsByIdAsync(categoryId);
+        }
+    }
+}
---
+++ NetNLayerApp.Service/Services/ProductService.cs
@@ -0,0 +1,24 @@
+﻿using NetNLayerApp.Core.Models;
+using NetNLayerApp.Core.Repositories;
+using NetNLayerApp.Core.Services;
+using NetNLayerApp.Core.UnitOfWorks;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetNLayerApp.Service.Services
+{
+    public class ProductService : Service<Product>, IProductService
+    {
+        public ProductService(IUnitOfWork unitOfWork, IRepository<Product> repository) : base(unitOfWork, repository)
+        {
+        }
+
+        public async Task<Product> GetWithCategoryByIdAsync(int productId)
+        {
+            return await _unitOfWork.Products.GetWithCategoryByIdAsync(productId);
+        }
+    }
+}
