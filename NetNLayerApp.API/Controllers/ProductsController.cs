ed146324bf30bf0bf5925519d8392cd48809dc6e
t4h4 <tahayasin@outlook.com>
2021-06-11 23:58:24 +0300

    Created ProductsController and added GetAll, GetById actions

---
+++ NetNLayerApp.API/Controllers/ProductsController.cs
@@ -0,0 +1,42 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using NetNLayerApp.API.DTOs;
+using NetNLayerApp.Core.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NetNLayerApp.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private readonly IMapper _mapper;
+        private readonly IProductService _productService;
+
+        public ProductsController(IProductService productService, IMapper mapper)
+        {
+            _productService = productService;
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            var products = await _productService.GetAllAsync();
+
+            return Ok(_mapper.Map<IEnumerable<ProductDto>>(products));
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetById(int id)
+        {
+            var product = await _productService.GetByIdAsync(id);
+
+            return Ok(_mapper.Map<ProductDto>(product));
+        }
+    }
+}
