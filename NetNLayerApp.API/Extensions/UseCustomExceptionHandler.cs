ffe6c79e5a317f7904fbfdcf88e7481ba2af5160
t4h4 <tahayasin@outlook.com>
2021-06-13 20:06:29 +0300

    Refactoring custom global level errors handle

---
+++ NetNLayerApp.API/Extensions/UseCustomExceptionHandler.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Http;
+using NetNLayerApp.API.DTOs;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NetNLayerApp.API.Extensions
+{
+    public static class UseCustomExceptionHandler
+    {
+        public static void UseCustomException(this IApplicationBuilder app) //IApplicationBuilder interface yapisina extensions method.(must be static class and method) (public void Configure(IApplicationBuilder app, IWebHostEnvironment env))
+        {
+            app.UseExceptionHandler(config =>
+            {
+                //hata firlatildiginda Run method ile calisilacak islemler silsilesi
+                config.Run(async context =>
+                {
+                    context.Response.StatusCode = 500; //internal server error
+                    context.Response.ContentType = "application/json";
+                    var error = context.Features.Get<IExceptionHandlerFeature>();//catch errors
+
+                    if (error != null)
+                    {
+                        var ex = error.Error;
+
+                        ErrorDto errorDto = new ErrorDto();
+                        errorDto.Status = 500;
+                        errorDto.Errors.Add(ex.Message);
+
+                        await context.Response.WriteAsync(JsonConvert.SerializeObject(errorDto));//errorDto objesi, json'a donusturulup response ediliyor.
+                    }
+                });
+            });
+        }
+    }
+}
--- NetNLayerApp.API/Startup.cs
+++ NetNLayerApp.API/Startup.cs
@@ -24,6 +24,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using NetNLayerApp.API.Extensions;
 
 namespace NetNLayerApp.API
 {
@@ -88,27 +89,7 @@ namespace NetNLayerApp.API
             }
 
             //middleware
-            app.UseExceptionHandler(config => 
-            {
-                //hata firlatildiginda Run method ile calisilacak islemler silsilesi
-                config.Run(async context =>
-                {
-                    context.Response.StatusCode = 500; //internal server error
-                    context.Response.ContentType = "application/json";
-                    var error = context.Features.Get<IExceptionHandlerFeature>(); //catch errors
-
-                    if (error != null)
-                    {
-                        var ex = error.Error;
-
-                        ErrorDto errorDto = new ErrorDto();
-                        errorDto.Status = 500;
-                        errorDto.Errors.Add(ex.Message);
-
-                        await context.Response.WriteAsync(JsonConvert.SerializeObject(errorDto)); //errorDto objesi, json'a donusturulup response ediliyor.
-                    }
-                });
-            });
+            app.UseCustomException();
 
             app.UseHttpsRedirection();
 
