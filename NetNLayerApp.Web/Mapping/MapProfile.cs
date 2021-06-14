dba907a1be04b9ad8be43cd60c50e1e4537b71ee
t4h4 <tahayasin@outlook.com>
2021-06-14 16:53:15 +0300

    Created dto's and map profile classes

---
+++ NetNLayerApp.Web/Mapping/MapProfile.cs
@@ -0,0 +1,29 @@
+﻿using AutoMapper;
+using NetNLayerApp.Web.DTOs;
+using NetNLayerApp.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NetNLayerApp.Web.Mapping
+{
+    public class MapProfile : Profile
+    {
+        public MapProfile()
+        {
+            //conversion
+            CreateMap<Category, CategoryDto>();
+            CreateMap<CategoryDto, Category>();
+
+            CreateMap<Category, CategoryWithProductDto>();
+            CreateMap<CategoryWithProductDto, Category>();
+
+            CreateMap<Product, ProductDto>();
+            CreateMap<ProductDto, Product>();
+
+            CreateMap<Product, ProductWithCategoryDto>();
+            CreateMap<ProductWithCategoryDto, Product>();
+        }
+    }
+}
